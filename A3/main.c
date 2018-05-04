211d3ac46a933a57a1cf06743233f89d9cbb8067
Unknown <s144146@student.dtu.dk>
2018-05-04 12:16:20 +0200

    A3 start
    sup!

--- A3/main.c
+++ A3/main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
+
+struct point {
+
+    int x,y;
+
+};
+
+struct lenght {
+
+
+
+};
+
+struct roof {
+
+    struct point point1;
+    struct point point2;
+    int length;
+
+};
+
+size_t getSize(float *ptr);
+
 int main() {
-    printf("Hello, World!\n");
+
+    int roofs[10] = {2,1};
+
+    printf("The bit space used in the array %u\n"
+           "The total bit space available  in the array %u\n"
+           "Bit space left %d\n"
+           "Objects in the array %d\n",getSize(roofs), sizeof(roofs),(sizeof(roofs)-getSize(roofs)), (getSize(roofs)/4));
+
+
+
     return 0;
 }
+
+size_t getSize(float *ptr) {
+
+    return sizeof(ptr);
+
+}
+
+
 
