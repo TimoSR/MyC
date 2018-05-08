21a77b9dba1b28fd5873c0a18208ea2d7ac05859
Unknown <s144146@student.dtu.dk>
2018-05-08 21:13:44 +0200

    WaterDrain

--- A3/main.c
+++ A3/main.c
@@ -11,12 +11,15 @@ typedef struct {
 
     POINT point1, point2;
     int length;
+    int water_Drain;
 
 } ROOF;
 
 ROOF roof_Info_wrt(int x1, int y1, int x2, int y2);
 
 void print_Roof(ROOF read);
+
+
 
 //size_t getSize(float *ptr);
 
@@ -30,14 +33,14 @@ int main() {
     int *number_of_roofs_ptr = &number_of_roofs;
 
     //Debugging
-    printf("How many roofs?\n");
+    //printf("How many roofs?\n");
 
     scanf("%d", &number_of_roofs);
 
     ROOF roof_Collection[*number_of_roofs_ptr];
 
     //Debugging
-    printf("Please enter coordinates\n");
+    //printf("Please enter coordinates\n");
 
     for (int i = 0; i < *number_of_roofs_ptr; ++i) {
 
@@ -46,23 +49,22 @@ int main() {
         roof_Collection[i] = roof_Info_wrt(x1, y1, x2, y2);
 
         //For debugging
-        print_Roof(roof_Collection[i]);
+        //print_Roof(roof_Collection[i]);
 
         if(0 < i) {
 
         for (int j = i; j > 0; j--) {
 
-            printf("i %d, j %d\n", i, j);
-
-            //if((j-1) < 0) {break;};
+            //Debugging
+            //printf("i %d, j %d\n", i, j);
 
             if(roof_Collection[j-1].point1.x > roof_Collection[j].point1.x) {
 
                 hold = roof_Collection[j-1];
                 roof_Collection[j-1] = roof_Collection[j];
-                print_Roof(roof_Collection[j-1]);
+                //print_Roof(roof_Collection[j-1]);
                 roof_Collection[j] = hold;
-                print_Roof(roof_Collection[j]);
+                //print_Roof(roof_Collection[j]);
 
                 };
             }
@@ -70,14 +72,48 @@ int main() {
         }
     }
 
-    printf("This is the roofs in descending order:\n");
-
+    //printf("This is the roofs in descending order:\n");
+    /*
     for (int k = 0; k < *number_of_roofs_ptr; k++) {
 
+        printf("roof %d\n", k+1);
         print_Roof(roof_Collection[k]);
 
     }
+    */
+    //printf("Calculating Water drain\n");
 
+    for (int l = 0; l < 2; ++l) {
+
+        int covered;
+
+        int exposed_To_Water;
+
+        if((l+1) > 2) { break;};
+
+        if(roof_Collection[l].point1.x <= roof_Collection[l+1].point1.x
+           && roof_Collection[l].point2.x >= roof_Collection[l+1].point1.x
+           && roof_Collection[l].point2.x <= roof_Collection[l+1].point2.x
+           && roof_Collection[l].point1.y < roof_Collection[l+1].point1.y
+           && roof_Collection[l].point2.y < roof_Collection[l+1].point2.y) {
+
+            covered = roof_Collection[l].point2.x - roof_Collection[l+1].point1.x;
+
+            printf("%d\n",covered);
+
+            exposed_To_Water = roof_Collection[l].length - covered;
+
+            printf("%d\n", exposed_To_Water);
+
+            roof_Collection[l].water_Drain = exposed_To_Water;
+
+            print_Roof(roof_Collection[l]);
+
+            }
+
+
+
+        }
 
 
 /**
@@ -105,9 +141,12 @@ void print_Roof(ROOF read) {
 
     printf("Point1 = x1: %d y1: %d\n"
            "Point2 = x2: %d y2: %d\n"
-           "Length = %d\n", read.point1.x, read.point1.y, read.point2.x, read.point2.y, read.length);
+           "Length = %d\n"
+           "Water Drain = %d\n", read.point1.x, read.point1.y, read.point2.x, read.point2.y, read.length, read.water_Drain);
 
 };
+
+
 
 /**
 size_t getSize(float *ptr) {
